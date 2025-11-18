6ee6894a67d90de72d0132144cbda0035e56b94e
Reecy-Z <66987237+Reecy-Z@users.noreply.github.com>
2025-11-18 18:43:16 +0800

    Update nicobot.py

--- NiCOBot/agents/nicobot.py
+++ NiCOBot/agents/nicobot.py
@@ -9,31 +9,21 @@ from langchain.prompts import PromptTemplate
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from pydantic import ValidationError
 from rmrkl import ChatZeroShotAgent, RetryAgentExecutor
+from langchain.agents import AgentType
+from langchain.agents import initialize_agent
 
 from .prompts import FORMAT_INSTRUCTIONS, QUESTION_PROMPT, REPHRASE_TEMPLATE, SUFFIX
 from .tools import make_tools
 
 def _make_llm(model, temp, api_key, streaming: bool = False):
-    if model.startswith("gpt-3.5-turbo") or model.startswith("gpt-4"):
-        llm = ChatOpenAI(
-            temperature=temp,
-            model_name=model,
-            request_timeout=1000,
-            streaming=streaming,
-            callbacks=[StreamingStdOutCallbackHandler()],
-            openai_api_key=api_key,
-            # openai_api_base=os.getenv("OPENAI_API_BASE"),
-        )
-    elif model.startswith("text-"):
-        llm = langchain.llms.OpenAI(
-            temperature=temp,
-            model_name=model,
-            streaming=streaming,
-            callbacks=[StreamingStdOutCallbackHandler()],
-            openai_api_key=api_key,
-        )
-    else:
-        raise ValueError(f"Invalid model name: {model}")
+    llm = ChatOpenAI(
+        temperature=temp,
+        model=model,
+        streaming=streaming,
+        callbacks=[StreamingStdOutCallbackHandler()],
+        openai_api_key=api_key,
+        openai_api_base=os.getenv("OPENAI_API_BASE"),
+    )
     return llm
 
 
@@ -41,13 +31,13 @@ class NiCOBot:
     def __init__(
         self,
         tools=None,
-        model="gpt-4-turbo",
-        tools_model="gpt-4-turbo",
+        model=os.getenv("MODEL"),
+        tools_model=os.getenv("MODEL"),
         temp=0.1,
         max_iterations=40,
         verbose=True,
         memory=None,
-        streaming: bool = True,
+        streaming: bool = False,
         openai_api_key: Optional[str] = None,
         api_keys: dict = {},
     ):
@@ -73,11 +63,25 @@ class NiCOBot:
                 format_instructions=FORMAT_INSTRUCTIONS,
                 question_prompt=QUESTION_PROMPT,
             ),
+    
             verbose=True,
             max_iterations=max_iterations,
             handle_parsing_errors=True,
             memory=memory,
         )
+
+        # self.agent_executor = initialize_agent(
+        #     agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,
+        #     llm=self.llm,
+        #     tools=tools,
+        #     suffix=SUFFIX,
+        #     format_instructions=FORMAT_INSTRUCTIONS,
+        #     question_prompt=QUESTION_PROMPT,
+        #     verbose=True,
+        #     max_iterations=max_iterations,
+        #     handle_parsing_errors=True,
+        #     memory=memory,
+        # )
 
         rephrase = PromptTemplate(
             input_variables=["question", "chat_history", "agent_ans"], template=REPHRASE_TEMPLATE
